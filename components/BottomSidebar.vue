baad5bfc29ce5aa933ffadf189e25da6566b0de5
BaxSTAR17 <baxford03@gmail.com>
2025-05-03 11:58:10 +0800

    Added Light Mode for mobile bars

--- components/BottomSidebar.vue
+++ components/BottomSidebar.vue
@@ -17,42 +17,42 @@
 </script>
 
 <template>
-    <div class="bg-neutral-900 min-h-20 w-full flex items-center">
+    <div class="bg-neutral-100 dark:bg-neutral-900 min-h-20 w-full flex items-center">
         <NuxtLink to="/" class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl hover:bg-neutral-800" v-show="useRoute().path !== '/'">
-            <UIcon name="i-basil-home-outline" size="30"/>
-            <span>Home</span>
+            <UIcon name="i-basil-home-outline" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="text-neutral-900 dark:text-neutral-100">Home</span>
         </NuxtLink>
         <div class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl" v-show="useRoute().path === '/'">
-            <UIcon name="i-basil-home-solid" size="30"/>
-            <span class="font-bold">Home</span>
+            <UIcon name="i-basil-home-solid" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="font-bold text-neutral-900 dark:text-neutral-100">Home</span>
         </div>
         <NuxtLink to="/favoritesmob" v-show="useRoute().path !== '/favoritesmob' && useRoute().path !== '/likes' && useRoute().path !== '/subscriptions'" class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl hover:bg-neutral-800">
-            <UIcon name="i-basil-star-outline" size="30"/>
-            <span>Favorites</span>
+            <UIcon name="i-basil-star-outline" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="text-neutral-900 dark:text-neutral-100">Favorites</span>
         </NuxtLink>
         <NuxtLink to="/favoritesmob" v-show="useRoute().path === '/favoritesmob' || useRoute().path === '/likes' || useRoute().path === '/subscriptions'" class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl hover:bg-neutral-800">
-            <UIcon name="i-basil-star-solid" size="30"/>
-            <span class="font-bold">Favorites</span>
+            <UIcon name="i-basil-star-solid" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="font-bold text-neutral-900 dark:text-neutral-100">Favorites</span>
         </NuxtLink>
         <NuxtLink to="/login" class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl hover:bg-neutral-800" v-if="guestMode === true">
-            <UIcon name="i-basil-user-outline" size="30"/>
-            <span>Log In</span>
+            <UIcon name="i-basil-user-outline" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="text-neutral-900 dark:text-neutral-100">Log In</span>
         </NuxtLink>
         <NuxtLink :to="`/profile/${id}`" class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl hover:bg-neutral-800" v-show="guestMode === false && useRoute().path !== `/profile/${id}`">
-            <UIcon name="i-basil-user-outline" size="30"/>
-            <span>Profile</span>
+            <UIcon name="i-basil-user-outline" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="text-neutral-900 dark:text-neutral-100">Profile</span>
         </NuxtLink>
         <NuxtLink :to="`/profile/${id}`" class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl hover:bg-neutral-800" v-show="guestMode === false && useRoute().path === `/profile/${id}`">
-            <UIcon name="i-basil-user-solid" size="30"/>
-            <span class="font-bold">Profile</span>
+            <UIcon name="i-basil-user-solid" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="font-bold text-neutral-900 dark:text-neutral-100">Profile</span>
         </NuxtLink>
         <NuxtLink to="/informationmob" v-show="useRoute().path !== '/informationmob' && useRoute().path !== '/faq' && useRoute().path !== '/about' && useRoute().path !== '/contact'" class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl hover:bg-neutral-800">
-            <UIcon name="i-basil-info-circle-outline" size="30"/>
-            <span>Information</span>
+            <UIcon name="i-basil-info-circle-outline" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="text-neutral-900 dark:text-neutral-100">Information</span>
         </NuxtLink>
         <NuxtLink to="/informationmob" v-show="useRoute().path === '/informationmob' || useRoute().path === '/faq' || useRoute().path === '/about' || useRoute().path === '/contact'" class="flex flex-col w-full min-h-20 justify-center items-center rounded-3xl hover:bg-neutral-800">
-            <UIcon name="i-basil-info-circle-solid" size="30"/>
-            <span class="font-bold">Information</span>
+            <UIcon name="i-basil-info-circle-solid" size="30" class="text-neutral-900 dark:text-neutral-100"/>
+            <span class="font-bold text-neutral-900 dark:text-neutral-100">Information</span>
         </NuxtLink>
     </div>
 </template>
--- components/TopSidebar.vue
+++ components/TopSidebar.vue
@@ -9,7 +9,7 @@
 </script>
 
 <template>
-    <div class="bg-neutral-900 min-h-20 w-screen flex justify-between items-center border-box pr-3">
+    <div class="bg-neutral-100 dark:bg-neutral-900 min-h-20 w-screen flex justify-between items-center border-box pr-3">
         <UTooltip :content="{align:'start'}" text="WAVY">
         <NuxtLink to="/" class="font-bold text-4xl leading-0">
             <img id="logo" class="hidden dark:block h-10" src='../public/WAVY Main Logo (White).svg' alt="Wavy Logo">
@@ -18,15 +18,15 @@
         </UTooltip>
         <div class="flex w-content min-h-20 items-center">
             <UTooltip :content="{align:'start'}" text="Upload">
-            <NuxtLink class="active:bg-neutral-800 w-15 h-20 flex items-center justify-center" to="/upload" v-show="guestMode === false && useRoute().path !== '/upload'"><UIcon name="i-uil-plus" size="45"/></NuxtLink>
+            <NuxtLink class="active:bg-neutral-800 w-15 h-20 flex items-center justify-center" to="/upload" v-show="guestMode === false && useRoute().path !== '/upload'"><UIcon name="i-uil-plus" size="45" class="text-neutral-900 dark:text-neutral-100"/></NuxtLink>
             </UTooltip>
             <UTooltip :content="{align:'start'}" text="Search">
-            <NuxtLink class="active:bg-neutral-800 w-15 h-20 flex items-center justify-center" to="/searchmob"><UIcon name="i-uil-search" size="40"/></NuxtLink>
+            <NuxtLink class="active:bg-neutral-800 w-15 h-20 flex items-center justify-center" to="/searchmob"><UIcon name="i-uil-search" size="40" class="text-neutral-900 dark:text-neutral-100"/></NuxtLink>
             </UTooltip>
             <UTooltip :content="{align:'start'}" text="Settings">
-            <NuxtLink class="active:bg-neutral-800 w-15 h-20 flex items-center justify-center" to="/settings" v-show="useRoute().path !== '/settings'"><UIcon name="i-basil-settings-alt-outline" size="42"/></NuxtLink>
+            <NuxtLink class="active:bg-neutral-800 w-15 h-20 flex items-center justify-center" to="/settings" v-show="useRoute().path !== '/settings'"><UIcon name="i-basil-settings-alt-outline" size="42" class="text-neutral-900 dark:text-neutral-100"/></NuxtLink>
             </UTooltip>
-            <NuxtLink class="active:bg-neutral-800 w-15 h-20 flex items-center justify-center" to="/settings" v-show="useRoute().path === '/settings'"><UIcon name="i-basil-settings-alt-solid" size="42"/></NuxtLink>
+            <NuxtLink class="active:bg-neutral-800 w-15 h-20 flex items-center justify-center" to="/settings" v-show="useRoute().path === '/settings'"><UIcon name="i-basil-settings-alt-solid" size="42" class="text-neutral-900 dark:text-neutral-100"/></NuxtLink>
         </div>
     </div>
 </template>
