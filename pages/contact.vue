bc4b9132c98f356166d1f9e1c2c2e4c08bd46b71
BaxSTAR17 <baxford03@gmail.com>
2025-05-07 15:45:10 +0800

    Added facebook page'

--- pages/contact.vue
+++ pages/contact.vue
@@ -48,7 +48,7 @@
             </p>
             <div class="flex flex-row mt-3">
                 <UTooltip :content="{align:'start'}" text="Facebook">
-                <NuxtLink class="mr-1" to="https://www.facebook.com/TanjayViners">
+                <NuxtLink class="mr-1" to="https://web.facebook.com/profile.php?id=61576064998968">
                     <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24"><path fill="BlueViolet" d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4z"/></svg>
                 </NuxtLink>
                 </UTooltip>
