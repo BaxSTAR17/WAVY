c7326c57688ce45a9614202babba24accd237b59
BaxSTAR17 <baxford03@gmail.com>
2025-05-08 22:26:39 +0800

    Logging condition

--- pages/index.vue
+++ pages/index.vue
@@ -64,10 +64,9 @@
         const mainpage = document.getElementById('homepage')
         mainpage.addEventListener("scroll", () => {
             if(mode.value === 'mode2' && (mainpage.scrollHeight - mainpage.scrollTop - mainpage.clientHeight) === 0 && payload.value < explore.value.length) {
-                if(payload.value < explore.value.length) {
-                    payload.value += 1
-                    exploree.value.push(explore.value[payload.value])
-                }
+                console.log("true")
+                payload.value += 1
+                if(payload.value < explore.value.length) exploree.value.push(explore.value[payload.value])
             }
             else if(mode.value === 'mode1' && user.value && (mainpage.scrollHeight - mainpage.scrollTop - mainpage.clientHeight) === 0 && fypload.value < foryou.value.length) fypload.value += 1
         })
